34166978ef06dc68c1169546094c3a152f9791e6
Christoph Kappel <christoph@unexist.dev>
2022-03-15 16:47:46 +0100

    Added init files

---
+++ Makefile
@@ -0,0 +1,80 @@
+define JSON_TODO_OK
+curl -X 'POST' \
+  'http://localhost:8080/todo' \
+  -H 'accept: */*' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "description": "string",
+  "done": true,
+  "dueDate": {
+    "due": "2021-05-07",
+    "start": "2021-05-07"
+  },
+  "title": "string"
+}'
+endef
+export JSON_TODO_OK
+
+define JSON_TODO_ERR
+curl -X 'POST' \
+  'http://localhost:8080/todo' \
+  -H 'accept: */*' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "description": "string",
+  "done": true,
+  "dueDate": {
+    "due": "2021-05-07",
+    "start": "2021-05-07"
+  },
+  "title": "badwordbadwordbadword"
+}'
+endef
+export JSON_TODO_ERR
+
+# Web
+open-kibana:
+	open http://localhost:5601
+
+open-elastic:
+	open http://localhost:9200
+
+open-jaeger:
+	open http://localhost:16686/
+
+# Quarkus
+service-create:
+	mvn -f todo-service-create/pom.xml quarkus:dev -P log-echopraxia,!log-sl4j
+
+service-verify:
+	mvn -f todo-service-verify/pom.xml quarkus:dev -P log-echopraxia,!log-sl4j
+
+service-store:
+	mvn -f todo-service-store/pom.xml quarkus:dev -P log-echopraxia,!log-sl4j
+
+services: service-create service-verify service-store
+
+# Tools
+rest-create-ok:
+	@echo $$JSON_TODO_OK | bash
+
+rest-create-err:
+	@echo $$JSON_TODO_ERR | bash
+
+rest-list:
+	@curl -X 'GET' 'http://localhost:8090/todo' -H 'accept: */*' | jq .
+
+rest-status:
+	@curl -X 'GET' 'http://localhost:8080/todo/$(id)' -H 'accept: */*' | jq .
+	@curl -X 'GET' 'http://localhost:8085/todo/$(id)' -H 'accept: */*' | jq .
+	@curl -X 'GET' 'http://localhost:8090/todo/$(id)' -H 'accept: */*' | jq .
+
+# Kafka
+kat-listen:
+	kcat -t todo_created -b localhost:9092 -C
+
+kat-test:
+	kcat -t todo_created -b localhost:9092 -P
+
+kt-listen:
+	kt consume -topic todo_created
---
+++ todo-service-verify/src/main/java/dev/unexist/showcase/todo/domain/todo/TodoService.java
@@ -0,0 +1,94 @@
+/**
+ * @package Quarkus-Logging-Tracing-Quarkus
+ *
+ * @file Todo service and domain service
+ * @copyright 2021-2022 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.domain.todo;
+
+import com.tersesystems.echopraxia.Logger;
+import com.tersesystems.echopraxia.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.awaitility.Awaitility.await;
+
+@ApplicationScoped
+public class TodoService {
+    private static final Logger<Todo.FieldBuilder> LOGGER = LoggerFactory.getLogger(TodoService.class)
+            .withFieldBuilder(Todo.FieldBuilder.class);
+
+    private static final List<String> BADWORDS = List.of("badword");
+
+    @Inject
+    TodoRepository todoRepository;
+
+    /**
+     * Check whether {@link Todo} contains any badwords
+     *
+     * @param  todo  A {@link Todo} entry
+     *
+     * @return Either {@code true} if a badword is found; otherwise {@code false}
+     **/
+
+    public boolean verify(Todo todo) {
+        await().between(Duration.ofSeconds(1), Duration.ofSeconds(10));
+
+        boolean invalid = Stream.of(todo.getTitle(), todo.getDescription())
+                        .anyMatch(part -> BADWORDS.stream()
+                                .anyMatch(part::contains));
+
+        LOGGER.info("Verified todo: {}, {}",
+                fb -> List.of(fb.todo("todo", todo),
+                        fb.bool("invalid", invalid)));
+        todo.setVerified(true);
+
+        return invalid;
+    }
+
+    /**
+     * Update {@link Todo} at with given id
+     *
+     * @param  todo  A {@link Todo} to update
+     *
+     * @return Either {@code true} on success; otherwise {@code false}
+     **/
+
+    public boolean update(Todo todo) {
+        boolean ret = false;
+
+        if (this.todoRepository.update(todo)) {
+            LOGGER.info("Updated todo: {}",
+                    fb -> fb.onlyTodo("todo", todo));
+
+            ret = true;
+        } else {
+            LOGGER.error("Cannot update todo: {}",
+                    fb -> List.of(fb.todo("todo", todo)));
+        }
+
+        return ret;
+    }
+
+    /**
+     * Find {@link Todo} by given id
+     *
+     * @param  id  Id to look for
+     *
+     * @return A {@link Optional} of the entry
+     **/
+
+    public Optional<Todo> findById(String id) {
+        return this.todoRepository.findById(id);
+    }
+}
