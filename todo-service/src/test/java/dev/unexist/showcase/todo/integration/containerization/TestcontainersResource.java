a0ce5157a4a12fa2b21c94e692591f26065cd2b5
Christoph Kappel <christoph@unexist.dev>
2022-04-15 18:38:11 +0200

    Switched to FixedHostPortGenericContainer

--- todo-service/src/test/java/dev/unexist/showcase/todo/integration/containerization/TestcontainersResource.java
+++ todo-service/src/test/java/dev/unexist/showcase/todo/integration/containerization/TestcontainersResource.java
@@ -12,30 +12,42 @@
 package dev.unexist.showcase.todo.integration.containerization;
 
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class TestcontainersResource implements QuarkusTestResourceLifecycleManager {
     private static final String SERVICE_URL = "localhost:8085";
     private static final String APP_PATH = "/opt/app";
-    private static final String JAR_FILE = "id-service-0.1.jar";
+    private static final String JAR_FILE = "id-service-0.1-runner.jar";
 
     private GenericContainer<?> container;
 
     @Override
     public Map<String, String> start() {
-        this.container = new GenericContainer(
+        Path jarPath = Paths.get(Paths.get("").toAbsolutePath().toString(), "/../id-service/target/", JAR_FILE);
+
+        GenericContainer<?> container = new GenericContainer(
                 new ImageFromDockerfile()
+                        .withFileFromPath(JAR_FILE, jarPath)
                         .withDockerfileFromBuilder(builder ->
                                 builder
                                         .from("eclipse-temurin:11")
-                                        .run("mkdir /opt/app")
-                                        .copy("../../../../../id-service/target/" + JAR_FILE, APP_PATH)
-                                        .cmd("java", "-jar", APP_PATH + JAR_FILE)
+                                        .run("mkdir " + APP_PATH)
+                                        .copy(JAR_FILE, APP_PATH)
+                                        .cmd("java", "-jar", Paths.get(APP_PATH, JAR_FILE).toString())
                                         .build()))
                 .withExposedPorts(8085);
+
+        this.container = new FixedHostPortGenericContainer(container.getDockerImageName())
+            .withFixedExposedPort(8085, 8085);
+
+        this.container.waitingFor(Wait.forListeningPort());
 
         this.container.start();
 
